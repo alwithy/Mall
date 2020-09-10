298e25f9d1900fa2b451e80d150475155373e430
alwithy <zxcvgvbnm@vip.qq.com>
2020-09-10 22:43:17 +0900

    使用Lombok自动生成get,set,toString方法

--- src/main/java/com/springboot/mall/dao/CategoryMapper.java
+++ src/main/java/com/springboot/mall/dao/CategoryMapper.java
@@ -1,7 +1,6 @@
 package com.springboot.mall.dao;
 
 import com.springboot.mall.pojo.Category;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
--- src/main/java/com/springboot/mall/pojo/Category.java
+++ src/main/java/com/springboot/mall/pojo/Category.java
@@ -1,11 +1,14 @@
 package com.springboot.mall.pojo;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /*
 * po(persistent object) 持久层对象
 * pojo(plian ordinary java object) 普通对象
 * */
+@Data
 public class Category {
 
     private Integer id;
@@ -22,72 +25,4 @@ public class Category {
 
     private Date updateTime;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Integer parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Integer getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(Integer sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", parentId=" + parentId +
-                ", name='" + name + '\'' +
-                ", status=" + status +
-                ", sortOrder=" + sortOrder +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
-    }
 }
