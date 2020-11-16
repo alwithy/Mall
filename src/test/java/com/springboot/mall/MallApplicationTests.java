5571e5d2bb0c756b085f982a54b7d7db41203372
alwithy <zxcvgvbnm@vip.qq.com>
2020-11-16 20:54:06 +0900

    update

--- src/test/java/com/springboot/mall/MallApplicationTests.java
+++ src/test/java/com/springboot/mall/MallApplicationTests.java
@@ -1,8 +1,8 @@
 package com.springboot.mall;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
